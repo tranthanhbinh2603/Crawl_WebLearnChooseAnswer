b31e21bae3743fa09ce95feedf7e5feac7621b9b
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2022-08-10 17:47:49 +0700

    Commit ngày 10/8/2022 lần 1

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,8 @@ namespace Crawl_WebLearnChooseAnswer
         {
             Func_WebRequest func = new Func_WebRequest();
             string link = textBox1.Text;
-            //List<Question> list = func.Crawler_Tech24h(link);
+            string title = "";
+            List<Question> list = func.Crawler_Tech12h(link, ref title);
             /*foreach (var item in list)
             {
                 richTextBox1.Text += item.question + "\n";
@@ -23,7 +24,7 @@ namespace Crawl_WebLearnChooseAnswer
                 richTextBox1.Text += item.correctAns + "\n\n";
             }*/
 
-            richTextBox1.Text = func.Crawler_DocTaiLieu(link);
+            //richTextBox1.Text = func.Crawler_DocTaiLieu(link);
 
             //https://cunghocvui.com/de-thi-kiem-tra/y97vzx94-trac-nghiem-toan-11-bai-3-mot-so-phuong-trinh-luong-giac-thuong-gap.html
             //richTextBox1.Text = func.Get_TrueAnswer_cungthionline(link);
